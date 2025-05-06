b1416ba6041bfea1ac7cbd9953d2277e0a761b7e
Carlos Arthur <carlos17.fsa15@gmail.com>
2025-05-06 20:26:34 -0300

    feat: handler for car

--- car/handler.go
+++ car/handler.go
@@ -1,26 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"sync"
 
+	"github.com/4r7hur0/PBL-2/schemas"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
 // Global list to store enterprise names
-var enterprises []string
+var enterprises []schemas.Enterprises
 var mu sync.Mutex // Mutex to handle concurrent access to the list
 
 // messageHandler processes incoming messages from the subscribed topic
 func messageHandler(client mqtt.Client, msg mqtt.Message) {
-	enterprise := string(msg.Payload())
+	var enterprise schemas.Enterprises
+
+	err := json.Unmarshal(msg.Payload(), &enterprise)
+	if err != nil {
+		fmt.Printf("Error deserializing message: %v\n", err)
+		return
+	}
 	addEnterprise(enterprise)
-	fmt.Printf("Received and stored enterprise: %s\n", enterprise)
+	fmt.Printf("Received and stored enterprise: %v\n", enterprise)
 }
 
 // addEnterprise adds an enterprise name to the global list and prints the list
-func addEnterprise(name string) {
+func addEnterprise(enterprise schemas.Enterprises) {
 	mu.Lock()
 	defer mu.Unlock()
-	enterprises = append(enterprises, name)
+	enterprises = append(enterprises, enterprise)
 }

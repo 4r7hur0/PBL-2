efc4b5695c75a716367a8f06677abb36f9d749e2
Carlos Arthur <carlos17.fsa15@gmail.com>
2025-05-06 13:59:03 -0300

    feat: api main code

---
+++ api/api.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/4r7hur0/PBL-2/api/mqtt"
+	"github.com/4r7hur0/PBL-2/api/router"
+)
+
+func main() {
+	// Initialize MQTT client
+	mqtt.InitializeMQTT("tcp://localhost:1883")
+
+	messageChannel := mqtt.StartListening("empresaA", 10)
+
+	go func() {
+		for message := range messageChannel {
+			// Process the incoming message
+			println("Received message: ", message)
+		}
+	}()
+
+	// Start the API router
+	router.InitRouter()
+}
